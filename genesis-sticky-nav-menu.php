746c0f2b8454290c14d84a76c5f8a0f79ad8502b
Russell Fair <rfair404@gmail.com>
2012-08-02 13:12:55 -0400

    version 005, fix textdomani
    Signed-off-by: Russell Fair <rfair404@gmail.com>

--- genesis-sticky-nav-menu.php
+++ genesis-sticky-nav-menu.php
@@ -2,22 +2,22 @@
 /*
 Plugin Name: Genesis Sticky Nav Menus with Contextual Sub Menu
 Description: Makes a Sticky Nav Menu with an optional Contextual Dropdown
-Version: 0.0.2
+Version: 0.0.5
 Plugin Author: Russell Fair
 Author URI: http://q21.co/
 */
 
 
 /*set up our constants*/
-define('GSNM_VER', '002');
-define('GSNM_TRANS_VER', '002');
+define('GSNM_VER', '005');
+define('GSNM_TRANS_VER', '005');
 define('GSNM_DIR', WP_PLUGIN_DIR.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)));
 define('GSNM_URL', WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)));
 
 
 
 // translation support
-load_plugin_textdomain( 'GSNM', false, '/genesis-sticky-nav-menu/languages/' );
+load_plugin_textdomain( 'sgnm', false, '/genesis-sticky-nav-menu/languages/' );
 
 require_once(GSNM_DIR . '/lib/gsnm-class-loader.php');
 
